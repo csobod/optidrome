c0bf1656b06655499651b0aa277649305fc2847d
csobod <chad.sobodash@gmail.com>
2023-03-30 19:46:34 -0700

    modified:   dbInitialize.py

--- dbInitialize.py
+++ dbInitialize.py
@@ -11,78 +11,114 @@
 ##############################################################################
 
 import sqlite3
+from sqlite3 import Error
 import os
-print(os.getcwd())
 
-# Creates tables user, patient, rxorder, frame, and lens.
 
-conn = sqlite3.connect('optidrome.db')
-c = conn.cursor()
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
 
-c.execute('''
-            CREATE TABLE IF NOT EXISTS optidrome.user (
-            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL
-)''')
+    return conn
 
-c.execute('''
-            CREATE TABLE IF NOT EXISTS optidrome.patient (
-            patient_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            first_name TEXT NOT NULL,
-            last_name TEXT NOT NULL,
-            email TEXT NOT NULL UNIQUE,
-            dob TEXT NOT NULL,
-            phone TEXT NOT NULL CHECK (length(phone) = 12),
-            street_address TEXT NOT NULL,
-            city TEXT NOT NULL,
-            state TEXT NOT NULL,
-            zip TEXT NOT NULL CHECK (length(zip) = 5)
-)''')
 
-c.execute('''
-            CREATE TABLE IF NOT EXISTS optidrome.rxorder (
-            order_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            patient_id INTEGER NOT NULL,
-            frame_id INTEGER NOT NULL,
-            lens_type TEXT NOT NULL,
-            lens_material TEXT NOT NULL,
-            lens_coating TEXT,
-            pd REAL NOT NULL,
-            origin_lab TEXT NOT NULL,
-            FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
-            FOREIGN KEY (frame_id) REFERENCES frame(frame_id),
-            FOREIGN KEY (origin_lab) REFERENCES lab(lab_name)
-)''')
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
 
-c.execute('''
-            CREATE TABLE IF NOT EXISTS optidrome.frame (
-            frame_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            frame_brand TEXT NOT NULL,
-            frame_model TEXT NOT NULL,
-            frame_color TEXT NOT NULL,
-            frame_material TEXT NOT NULL,
-            frame_shape TEXT NOT NULL,
-            frame_style TEXT NOT NULL
-)''')
 
-c.execute('''
-            CREATE TABLE IF NOT EXISTS optidrome.lens (
-            lens_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            lens_type TEXT NOT NULL,
-            lens_material TEXT NOT NULL,
-            lens_coating TEXT NOT NULL
-)''')
+def main():
+    database = "optidrome.db"
+    
+    sql_create_user_table = """
+        CREATE TABLE IF NOT EXISTS optidrome.user (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+    ); """
 
-c.execute('''
-            CREATE TABLE IF NOT EXISTS optidrome.lab (
-            lab_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            lab_name TEXT NOT NULL,
-            lab_address TEXT NOT NULL,
-            lab_phone TEXT NOT NULL CHECK (length(phone) = 12),
-            lab_fax TEXT NOT NULL CHECK (length(phone) = 12),
-            lab_email TEXT NOT NULL UNIQUE,
-)''')
+    sql_create_patient_table = """
+        CREATE TABLE IF NOT EXISTS optidrome.patient (
+        patient_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        dob TEXT NOT NULL,
+        phone TEXT NOT NULL CHECK (length(phone) = 12),
+        street_address TEXT NOT NULL,
+        city TEXT NOT NULL,
+        state TEXT NOT NULL,
+        zip TEXT NOT NULL CHECK (length(zip) = 5)
+    ); """
 
-conn.commit()
-conn.close()
+    sql_create_rxorder_table = """
+        CREATE TABLE IF NOT EXISTS optidrome.rxorder (
+        order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        patient_id INTEGER NOT NULL,
+        frame_id INTEGER NOT NULL,
+        lens_type TEXT NOT NULL,
+        lens_material TEXT NOT NULL,
+        lens_coating TEXT,
+        pd REAL NOT NULL,
+        origin_lab TEXT NOT NULL,
+        FOREIGN KEY (patient_id) REFERENCES patient(patient_id),
+        FOREIGN KEY (frame_id) REFERENCES frame(frame_id),
+        FOREIGN KEY (origin_lab) REFERENCES lab(lab_name)
+    ); """
+
+    sql_create_frame_table = """
+        CREATE TABLE IF NOT EXISTS optidrome.frame (
+        frame_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        frame_brand TEXT NOT NULL,
+        frame_model TEXT NOT NULL,
+        frame_color TEXT NOT NULL,
+        frame_material TEXT NOT NULL,
+        frame_shape TEXT NOT NULL,
+        frame_style TEXT NOT NULL
+    ); """
+
+    sql_create_lens_table = """
+        CREATE TABLE IF NOT EXISTS optidrome.lens (
+        lens_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        lens_type TEXT NOT NULL,
+        lens_material TEXT NOT NULL,
+        lens_coating TEXT NOT NULL
+    ); """
+
+    sql_create_lab_table = """
+        CREATE TABLE IF NOT EXISTS optidrome.lab (
+        lab_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        lab_name TEXT NOT NULL,
+        lab_address TEXT NOT NULL,
+        lab_phone TEXT NOT NULL CHECK (length(phone) = 12),
+        lab_fax TEXT NOT NULL CHECK (length(phone) = 12),
+        lab_email TEXT NOT NULL UNIQUE,
+    ); """
+
+    conn = create_connection(database)
+
+    if conn is not None:
+        create_table(conn, sql_create_user_table)
+        create_table(conn, sql_create_patient_table)
+        create_table(conn, sql_create_rxorder_table)
+        create_table(conn, sql_create_frame_table)
+        create_table(conn, sql_create_lens_table)
+        create_table(conn, sql_create_lab_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
